df134964a414e6c878c1847a1a37716c4c36dca8
Dmitry Patsura <zaets28rus@gmail.com>
2016-03-01 14:44:12 +0300

    [Working] Decode video frames (pictures)

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ int main() {
     }
 
     av_dump_format(inputFormatContext, 0, nullptr, 0);
-    
+
     for (int i = 0; i < inputFormatContext->nb_streams; i++) {
         auto *currentStream = inputFormatContext->streams[i];
         if (currentStream->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
@@ -53,9 +53,69 @@ int main() {
     cout << "Index " << videoStream->index << endl;
     cout << "Id " << videoStream->id << endl;
 
-    auto videoFrame = av_frame_alloc();
-    auto videoFrameRGB = av_frame_alloc();
 
+    /**
+     * Исходный кадр
+     */
+    AVFrame *videoFrame = nullptr;
+    videoFrame = av_frame_alloc();
+    if (videoFrame == nullptr) {
+        cout << "Cannot allocate memory for videoFrame for next decoding for it" << endl;
+        return 0;
+    }
+
+    AVFrame *videoFrameRGB;
+    videoFrameRGB = av_frame_alloc();
+    if (videoFrameRGB == nullptr) {
+        cout << "Cannot allocate memory for videoFrameRGB for next converting for it" << endl;
+        return 0;
+    }
+
+    auto swContext = sws_getContext(
+        videoStream->codec->width,
+        videoStream->codec->height,
+        videoStream->codec->pix_fmt,
+        videoStream->codec->width,
+        videoStream->codec->height,
+        PIX_FMT_RGB24,
+        0,
+        nullptr,
+        nullptr,
+        nullptr
+    );
+
+
+    AVPacket *packet;
+    int frameFinished;
+    packet = new(AVPacket);
+
+    while (true) {
+        cout << "Read packet..." << endl;
+
+        code = av_read_frame(inputFormatContext, packet);
+        if (code < 0) {
+            if (code == AVERROR_EOF) {
+                /** It is the of file */
+                break;
+            } else {
+                cout << av_err2str(code) << endl;
+                return 0;
+            }
+        }
+
+        /** This packet is a packet from video stream (Frame) */
+        if (packet->stream_index == videoStream->index) {
+            /**
+             * @return On error a negative value is returned, otherwise the number of bytes
+             * used or zero if no frame could be decompressed.
+             * */
+            code = avcodec_decode_video2(videoStream->codec, videoFrame, &frameFinished, packet);
+
+            if (frameFinished > 0) {
+                
+            }
+        }
+    }
 
     return 0;
 }
