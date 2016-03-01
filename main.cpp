1b08b35a78a1a0a4d2b99f6eed1e36061d6514b6
Dmitry Patsura <zaets28rus@gmail.com>
2016-03-01 14:30:47 +0300

    [Working] Find videoStream

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
     string filename = "/Users/ovr/ClionProjects/ffmpeg-test/1.mp4";
-    AVStream *videoStream;
+    AVStream *videoStream = nullptr;
     int code;
 
     av_register_all();
@@ -24,6 +24,7 @@ int main() {
     code = avformat_open_input(&inputFormatContext, filename.c_str(), nullptr, nullptr);
     if (code < 0) {
         cout << "Error on avformat_open_input was cause with code " << av_err2str(code) << endl;
+        return 0;
     }
 
 
@@ -31,10 +32,26 @@ int main() {
     code = avformat_find_stream_info(inputFormatContext, nullptr);
     if (code < 0) {
         cout << "Error on avformat_find_stream_info was cause with code " << av_err2str(code) << endl;
+        return 0;
     }
 
     av_dump_format(inputFormatContext, 0, nullptr, 0);
+    
+    for (int i = 0; i < inputFormatContext->nb_streams; i++) {
+        auto *currentStream = inputFormatContext->streams[i];
+        if (currentStream->codec->codec_type == AVMEDIA_TYPE_VIDEO) {
+            videoStream = currentStream;
+        }
+    }
 
+    if (videoStream == nullptr) {
+        cout << "No video stream :(" << endl;
+        return 0;
+    }
+
+    cout << "Video Stream" << endl;
+    cout << "Index " << videoStream->index << endl;
+    cout << "Id " << videoStream->id << endl;
 
     auto videoFrame = av_frame_alloc();
     auto videoFrameRGB = av_frame_alloc();
